8ef9dd0707482121fd61070000425706d1c73940
Michael Wales <mwales3@gmail.com>
2026-01-28 02:40:12 -0500

    tebov compressing, encrypting, and transfering files to remote server

--- tools/common_backup_utils.py
+++ tools/common_backup_utils.py
@@ -9,7 +9,7 @@ import struct
 import stat
 
 
-from typing import Tuple, Dict, Set
+from typing import List, Tuple, Dict, Set
 
 # apt-get install python3-pycryptodome
 from Cryptodome.Hash import SHA256, MD5
@@ -18,16 +18,16 @@ from Cryptodome.Cipher import AES
 
 class ConfigFile:
 
-    required_config = [ 
+    required_config: List [ str ] = [ 
         "BACKUP_SERVER",
         "BACKUP_SERVER_USER",
         "BACKUP_SERVER_BACKUP_PATH",
         "BACKUP_SERVER_INDEX_PATH",
         "LOCAL_INDEX_PATH",
-        "LOCAL_PATH_TO_BACKUP" ]
+        "LOCAL_PATH_TO_BACKUP",
+        "ENCRYPT_PASSWORD" ]
 
-    def __init__(self, filepath):
-
+    def __init__(self, filepath: str):
         
         self.config = {}
         try:
@@ -46,40 +46,95 @@ class ConfigFile:
                 continue
             self.config[tokens[0]] = tokens[1]
 
-    def is_required_config_available(self):
+    def is_required_config_available(self) -> bool:
         reqd = set(ConfigFile.required_config)
         present = set(self.config.keys())
 
         missing = reqd - present
         if missing:
-            write(sys.stderr, "Missing configuration: {missing}")
+            sys.stderr.write(f"Missing configuration: {missing}\n")
             return False
         else:
             return True
 
     @classmethod
-    def print_configuration_sample(cls):
+    def print_configuration_sample(cls) -> None:
         print("Sample configuration file contents:")
         print(" ")
         for key in ConfigFile.required_config:
             print(f"{key}=VALUE")
 
-    def get(self, key):
+    def get(self, key) -> str:
         return self.config[key]
 
 class BackupUtils:
-    @classmethod
-    def get_file_list(cls, directory_path):
+   
+    no_compress_ext_list = [ ".zip", ".tgz", ".tar.gz", ".bz2", ".jpg",
+                             ".jpeg", ".mp4", ".mp3", ".flac", ".7z", ".png", 
+                             ".webm"
+                             ]
+
+    def __init__(self, config_file):
+        self.config = ConfigFile(config_file)
+        
+        self.password = self.config.get("ENCRYPT_PASSWORD")
+        self.key = CryptoUtils.deriveKey(self.password)
+
+        self.server_hostname = self.config.get("BACKUP_SERVER")
+        self.server_username = self.config.get("BACKUP_SERVER_USER")
+        self.server_remote_folder = self.config.get("BACKUP_SERVER_BACKUP_PATH")
+
+        directory_path = self.config.get("LOCAL_PATH_TO_BACKUP")
+        self.backup_path_normal = os.path.normpath(directory_path) + "/"
+
+        self.pt_hash_cache: Dict [ bytes, bytes ] = {}
+
+        self.file_list: List [ str ] = []
+
+        self.filename_pt_hash: Dict [ str, bytes ]
+
+        self.logger = logging.getLogger("BackupUtil")
+
+    def backup_files(self, backup_name):
+
+        rs = RemoteServer(self.server_hostname, self.server_username, self.server_remote_folder)
+
+        for fn in self.file_list:
+            dontCompress = False
+            try:
+                file_ext_start = fn.rindex('.')
+                ext = fn[file_ext_start:]
+                logging.debug(f"ext searching for {ext}")
+                if ext in BackupUtils.no_compress_ext_list:
+                    dontCompress = True
+            except:
+                logging.debug(f"Filename {fn} has no extension")
+                pass
+
+            if dontCompress:
+                rs.crypt_txfer(self.key, os.path.join(self.backup_path_normal, fn))
+            else:
+                rs.compress_crypt_txfer(self.key, os.path.join(self.backup_path_normal, fn))
+
+    def get_file_list(self):
         """
         Can't use built in glob stuff because I don't want to follow sym links
         """
-        retval = []
+
+        directory_path = self.config.get("LOCAL_PATH_TO_BACKUP")
+        self.backup_path_normal = os.path.normpath(directory_path) + "/"
+
+        self.file_list = []
         for dirpath, dirnames, filenames in os.walk(directory_path, followlinks=False):
 
             for fn in filenames:
-                retval.append(os.path.join(dirpath, fn))
+                full_filename = os.path.normpath(os.path.join(dirpath, fn))
+                rel_filename = full_filename.replace(self.backup_path_normal, "")
 
-        return retval
+                self.file_list.append(rel_filename)
+                #self.logger.debug(f"Adding file: {os.path.join(dirpath, fn)}")
+                self.logger.debug(f"Adding file: {rel_filename}")
+
 
 class CryptoUtils:
     chunkSize = 16 * 1024 * 1024 # 16MB
@@ -111,7 +166,7 @@ class CryptoUtils:
     def encryptBytes(cls, key: bytes, inbytes: bytes):
         ctx = AES.new(key, AES.MODE_SIV)
         ct, tag = ctx.encrypt_and_digest(inbytes)
-        print(f"Length of ct = {len(ct)} and tag = {len(tag)}")
+        logging.debug(f"Length of ct = {len(ct)} and tag = {len(tag)}")
         return tag + ct
 
     @classmethod
@@ -261,6 +316,7 @@ class CryptoUtils:
         output_file.close()
 
         hash_value = hash_ctx.digest()
+        logging.debug(f"compress_encrypt_file encrypted a file, ct hash = {hash_value.hex()}, {total_bytes_read} -> {total_bytes_written}")
         return hash_value, total_bytes_read, total_bytes_written
 
     @classmethod
@@ -269,7 +325,6 @@ class CryptoUtils:
 
         total_bytes_read = 0
         total_bytes_written = 0
-        compressed_data = bytes()
         while True: 
             chunk = input_file.read(CryptoUtils.chunkSize)
 
@@ -277,13 +332,12 @@ class CryptoUtils:
                 break
 
             total_bytes_read += len(chunk)
-            ct = CryptoUtils.encryptBytes(key, compressed_data)
+            ct = CryptoUtils.encryptBytes(key, chunk)
 
             len_bytes = struct.pack(">I", len(ct))
 
             output_file.write(len_bytes + ct)
             hash_ctx.update(len_bytes + ct)
-
             total_bytes_written += len(ct) + 4
 
         # Edge case, empty file
@@ -295,30 +349,10 @@ class CryptoUtils:
             return None, 0, 0
 
         input_file.close()
-
-        compressed_data += compressor.flush()
-        while len(compressed_data) > 0:
-    
-            if len(compressed_data) > CryptoUtils.chunkSize:
-                ct = CryptoUtils.encryptBytes(key, compressed_data[:CryptoUtils.chunkSize])
-                compressed_data = compressed_data[CryptoUtils.chunkSize:]
-            else:
-                ct = CryptoUtils.encryptBytes(key, compressed_data)
-                compressed_data = bytes()
-
-            len_bytes = struct.pack(">I", len(ct))
-
-            output_file.write(len_bytes + ct)
-            hash_ctx.update(len_bytes + ct)
-
-            total_bytes_written += len(ct) + 4
-
         output_file.close()
         hash_value = hash_ctx.digest()
+        logging.debug(f"encrypt_file encrypted a file, ct hash = {hash_value.hex()}, {total_bytes_read} -> {total_bytes_written}")
         return hash_value, total_bytes_read, total_bytes_written
-
-
-
 
 
 class RemoteServer:
@@ -347,11 +381,9 @@ class RemoteServer:
 
     def get_remote_file_size(self, filename: str):
         self.logger.debug(f"Getting remote filesize of {filename}")
-        remote_full_path = self.remote_folder + "/" + filename
-        self.logger.debug(f"remote full path {remote_full_path}")
         
         try:
-            st = self.sftp.stat(remote_full_path)
+            st = self.sftp.stat(filename)
             self.logger.debug(f"st = {st}")
             return st.st_size
         except Exception as e:
@@ -396,7 +428,16 @@ class RemoteServer:
     def move_remote_file_mkpath(self, original_path, new_path, filename):
         # Lets build the path if it isn't already there
         self.make_path(new_path)
-        self.sftp.rename(self.remote_folder + "/" + original_path, self.remote_folder + "/" + new_path + "/" + filename)
+        new_dest_path = self.remote_folder + "/" + new_path + "/" + filename
+
+        remote_size = self.get_remote_file_size(new_dest_path)
+        if remote_size > 0:
+            self.logger.debug(f"Can't move file to {new_dest_path}, file already exists, deleting {original_path}")
+            self.sftp.remove(self.remote_folder + "/" + original_path)
+        else: 
+            original_full_path = os.path.join(self.remote_folder, original_path)
+            self.logger.debug(f"SFTP moving {original_full_path} to {new_dest_path}")
+            self.sftp.rename(self.remote_folder + "/" + original_path, new_dest_path)
         
 
     @classmethod
@@ -407,6 +448,7 @@ class RemoteServer:
         return (backup_folders, final_name)
 
     def compress_crypt_txfer(self, key: bytes, in_path: str):
+        self.logger.debug(f"compress_crypt_txfer {in_path}")
         input_file = open(in_path, "rb")
 
         temp_filename = "txfer_in_progress"
@@ -422,9 +464,12 @@ class RemoteServer:
         logging.debug(f"Moving {temp_filename} to {backup_folders}/{final_name}")
         self.move_remote_file_mkpath(temp_filename, backup_folders, final_name)
 
+        ratio = round( (total_bytes_read - total_bytes_written) / total_bytes_read * 100.0, 1)
+        print(f"Backed up {in_path} [ Compressed {total_bytes_read} to {total_bytes_written} {ratio}% ]")
         return hash_value, total_bytes_read, total_bytes_written
 
     def crypt_txfer(self, key: bytes, in_path: str):
+        self.logger.debug(f"crypt_txfer {in_path}")
         input_file = open(in_path, "rb")
 
         temp_filename = "txfer_in_progress"
@@ -440,6 +485,7 @@ class RemoteServer:
         logging.debug(f"Moving {temp_filename} to {backup_folders}/{final_name}")
         self.move_remote_file_mkpath(temp_filename, backup_folders, final_name)
 
+        print(f"Backed up {in_path} [ Uncompressed {total_bytes_read} to {total_bytes_written} ]")
         return hash_value, total_bytes_read, total_bytes_written
 
 
